fdc05ef1cba53980dfd7e49e109fa1e1ad93c588
AnnaHex <A.A.Beletskaja@gmail.com>
2023-11-19 18:35:52 +0300

    add

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,4 +4,6 @@ const { program } = require('commander');
 program
   .version('0.2.1')
   .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'Output format', 'stylish')
+  .arguments('<firstConfig> <secondConfig>')
   program.parse();
